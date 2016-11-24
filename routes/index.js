032efefa1ebcc112f0f410af6d1b333ef7686dc4
Ghanshyam <ghanshyam.dobariya@quikr.com>
2016-11-24 13:50:50 +0530

    added css and sample content

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res){
+  res.render('index', {
+    title: 'Welcome to Quikr SME Platform'
+  });
+});
+
+router.get('/about', function(req, res){
+  res.render('about', {
+    title: 'Know more about Quikr SME Platform'
+  });
+});
+
+router.get('/contact', function(req, res){
+  res.render('contact', {
+    title: 'Contact Us'
+  });
 });
 
 module.exports = router;
